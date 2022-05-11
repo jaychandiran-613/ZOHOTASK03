54621dae5f1ec44adf87292a413fbc44ea941542
jaychandiran-613 <jayachandiran613@gmail.com>
2022-05-11 20:06:39 +0530

    waiting feature added

--- fifoclient.cpp
+++ fifoclient.cpp
@@ -9,10 +9,15 @@ int main(void)
     DWORD dwWritten,dwRead;
     char sample[50],buffer[1024];
     int option = 0;
-
-
-
-    hPipe = CreateFile(TEXT("\\\\.\\pipe\\MYFIFO"), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+    
+    for(int i=0;i<10;i++)
+    {   hPipe = CreateFile(TEXT("\\\\.\\pipe\\MYFIFO"), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+        if(hPipe!=INVALID_HANDLE_VALUE)
+        {
+            break;
+        }
+        Sleep(1000);
+    }
     if (hPipe != INVALID_HANDLE_VALUE)
     {
         while(1)
@@ -22,6 +27,10 @@ int main(void)
             cin>>sample;
             WriteFile(hPipe, sample,sizeof(sample), &dwWritten, NULL);  
             option = 1;  
+        }
+        if(strcmp(sample,"end")==0)
+        {
+            break;
         }
         if(ReadFile(hPipe, buffer, sizeof(buffer) - 1, &dwRead, NULL) != FALSE && option == 1)
         {
@@ -33,6 +42,95 @@ int main(void)
 
     }
     
+    cout<<"The connection is broken.\n";
     CloseHandle(hPipe);
     return (0);
 }
+
+
+/*
+#include<iostream>
+#include <windows.h> 
+#include<string.h>
+using namespace std;
+#define FIFO_FILE "MYFIFO"
+class CLIENT{
+    private:
+    HANDLE hPipe;
+    DWORD dwWritten,dwRead;
+    char buffer[1024],sample[50];
+    public:
+    void createpipe()
+    {
+       hPipe = CreateFile(TEXT("\\\\.\\pipe\\MYFIFO"), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+     }
+    bool checkfile()
+    {
+        
+        return hPipe;
+    }
+    void write()
+    {   
+        cout<<"enter your message   : ";
+        cin>>sample;
+        WriteFile(hPipe, sample,sizeof(sample), &dwWritten, NULL);
+    }
+    int checksample()
+    {
+        return strcmp(sample,"end");
+    }
+    string read()
+    {
+        ReadFile(hPipe, buffer, sizeof(buffer) - 1, &dwRead, NULL);
+        buffer[dwRead] = '\0';
+        return buffer;
+    }
+    void closeconnection()
+    {
+        CloseHandle(hPipe);
+    }
+
+};
+int main()
+{
+    int option = 0;
+    CLIENT c;
+    char sample[50];
+    string buffer;
+    for(int i=0;i<10;i++)
+    {   c.createpipe();
+        if(c.checkfile() != FALSE)
+        {
+            break;
+        }
+        Sleep(1000);
+    }
+    if (c.checkfile() != FALSE)
+    {
+        while(1)
+        {
+        if(option == 0)
+        {   
+            c.write();
+            option = 1;  
+        }
+        if(c.checksample() ==0)
+        {
+            break;
+        }
+        if(option == 1)
+        {
+            buffer = c.read();
+            cout<<"The received message : "<<buffer<<endl;
+            option = 0;
+        }
+        }
+
+    }
+    
+    cout<<"The connection is broken.\n";
+    c.closeconnection();
+    return (0);
+}
+
+*/
--- fifoserver.cpp
+++ fifoserver.cpp
@@ -1,42 +1,94 @@
 #include<iostream>
+#include <unistd.h>
 #include <windows.h> 
 #include<string>
 using namespace std;
 #define FIFO_FILE "MYFIFO"
-int main(void)
-{
+class SERVER{
+    private:
     HANDLE hPipe;
     char buffer[1024];
+    char sample[50];
     DWORD dwRead,dwWritten;
     int option = 0;
-    char sample[50];
-
-    hPipe = CreateNamedPipe(TEXT("\\\\.\\pipe\\MYFIFO"),PIPE_ACCESS_DUPLEX, PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT, 1, 1024 * 16, 1024 * 16, NMPWAIT_USE_DEFAULT_WAIT, NULL);
-    while (hPipe != INVALID_HANDLE_VALUE)
+    bool check = TRUE;
+    public:
+    SERVER(string a,string b,string c,string d)
     {
-        if (ConnectNamedPipe(hPipe, NULL) != FALSE)   // wait for someone to connect to the pipe
-        {
-            while (1)
-            {
-                if(ReadFile(hPipe, buffer, sizeof(buffer) - 1, &dwRead, NULL) != FALSE && option == 0)
-                {
-                    buffer[dwRead] = '\0';
-                    cout<<"The received message : "<<buffer<<endl;
-                    option = 1;
-                }
-
-                if(option == 1)
-                {
-                    cout<<"enter your message   : ";
-                    cin>>sample;
-                    WriteFile(hPipe,sample,sizeof(sample), &dwWritten, NULL);
-                    option = 0;
-                }
-            }
-        }
-
+        hPipe = CreateNamedPipe(TEXT("\\\\.\\pipe\\MYFIFO"),PIPE_ACCESS_DUPLEX, PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT, 1, 1024 * 16, 1024 * 16, NMPWAIT_USE_DEFAULT_WAIT, NULL);
+    }
+    bool checkpipe()
+    {
+        return hPipe;
+    }
+    bool checkconnection()
+    {
+        return ConnectNamedPipe(hPipe, NULL);    
+    }
+    string checkread()
+    {
+        ReadFile(hPipe, buffer, sizeof(buffer) - 1, &dwRead, NULL);
+        buffer[dwRead] = '\0';
+        return buffer;
+    }
+    void checkwrite()
+    {
+        cout<<"enter your message   : ";
+        cin>>sample;
+        WriteFile(hPipe,sample,sizeof(sample), &dwWritten, NULL);
+    }
+    int checksample()
+    {
+        return strcmp(sample,"end");
+    }
+    void closehandle()
+    {
+        CloseHandle(hPipe);
+    }
+    void connection()
+    {
         DisconnectNamedPipe(hPipe);
     }
 
+};
+int main(void)
+{   int option =0;
+    string buffer;
+    SERVER s("PIPE_ACCESS_DUPLEX","PIPE_TYPE_BYTE", "PIPE_READMODE_BYTE", "PIPE_WAIT");
+           if(s.checkpipe() != 0 )//&& sleep(10) && ConnectNamedPipe(hPipe, NULL) != FALSE
+    {
+            for(int i=0;i<5;i++)
+            {
+                if(s.checkconnection() == TRUE)
+                {
+                    cout<<"server is active\n";
+                    break;
+                }
+                sleep(1000);
+                cout<<i<<endl;
+            }  
+            while (1)
+            {   
+                if(option == 0)
+                {
+                    buffer = s.checkread();
+                    cout<<"The received message : "<<buffer<<endl;
+                    option = 1;
+                }
+                if(option == 1)
+                {
+                    s.checkwrite();
+                    option = 0;
+                }
+                if(s.checksample()==0)
+                {   
+                    break;
+                }
+            }
+            
+    }
+        cout<<"The connection is broken.\n";
+        s.closehandle();
+        s.connection();
     return 0;
 }
